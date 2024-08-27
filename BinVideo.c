4924760e0d0905264357474d421b7eb8c782ddff
Josep Audenis <josep.audenis@students.salle.url.edu>
2024-08-27 16:05:16 +0200

    conversors de binari-decimal

--- BinVideo.c
+++ BinVideo.c
@@ -6,15 +6,47 @@
 
 #define WIDTH 640
 #define HEIGHT 480
+//(WIDTH x HEIGHT) % 8 = 0
 #define BYTE_SIZE 8
 #define FRAME_BYTES 640*480
 #define VIDEO_EXTENSION ".mp4"
+#define BINARY_SIZE 40
 
+
+int* decimalToBinary(int decimal){
+    int* binary = (int*)malloc(BINARY_SIZE * sizeof(int));
+    int i = 0;
+
+    while(decimal > 0){
+        binary[i] = decimal % 2;
+        decimal /= 2;
+        i++;
+    }
+
+    while(i < 40){
+        binary[i] = 0;
+        i++;
+    }
+
+    return binary;
+}
+
+int binaryToDecimal(/*passa array del binary*/){
+    int decimal = 0;
+    int power = 1;
+
+    for(int i = 0; i < BINARY_SIZE; i++){
+        decimal += binary[i] * power;
+        power *= 2;  
+    }
+
+    return decimal;
+}
 
 //Funcio que donat el nom d'un fitxer parsejar el seu valor binari i el converteix en un video
 void file_to_video(const char *input_file, const char *output_file){
     
-    FILE *file = fopen(input_file, "rb");
+    FILE *fil e = fopen(input_file, "rb");
     if (file == NULL){
         perror("Error opening file %s", input_file);
         return;
@@ -62,6 +94,9 @@ void file_to_video(const char *input_file, const char *output_file){
 
     //al guardar el nom del video es guardara no el nom que es demana sino que es guardara amb el següent format: nom del fitxer original_format del fitxer original_tamany del fitxer original en bytes.mp4
     //de cara al futur es podria mirar de reservar els primers bits del video a aquesta informacio
+    //- Reservar els 40 primers bits per al tamany original (aprox)
+    //- Reservar els 4 següents bits per a l'extensio original
+
 
 }
 
@@ -71,7 +106,15 @@ void video_to_file(){
 
 int main(){
 
+
+    int* binary = decimalToBinary(/*number*/);
+    for (int i = 40; i > 0; i--){
+        printf("\n%d", binary[i-1]);
+    }
+    int decimal = binaryToDecimal(binary);
+    printf("\n%d", decimal);
     
 
+    free(binary);
     return 0;
 }
